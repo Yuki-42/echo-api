7108439b7cc6d10fc0c7323d7713851141c7ea32
Yuki-42 <kylepannan@gmail.com>
2024-07-09 13:02:42 +1000

    More work on schema

--- schema.sql
+++ schema.sql
@@ -24,16 +24,63 @@ CREATE TABLE public.users (
     username TEXT NOT NULL
 );
 
+CREATE TABLE public.files (
+    id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),  /* Id is also the filename */
+    created_at TIMESTAMP NOT NULL DEFAULT current_timestamp,
+    created_by uuid NOT NULL
+);
+
 CREATE TABLE public.guilds (
-    
+    id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
+    created_at TIMESTAMP NOT NULL DEFAULT current_timestamp,
+    owner_id uuid NOT NULL,
+    name TEXT NOT NULL,
+    icon uuid
 );
 
 CREATE TABLE public.channels(
-
+    id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
+    created_at TIMESTAMP NOT NULL DEFAULT current_timestamp,
+    guild_id uuid NOT NULL,
+    name TEXT NOT NULL,
+    type INT NOT NULL,
+    parent uuid
 );
 
 CREATE TABLE public.messages(
+    id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
+    created_at TIMESTAMP NOT NULL DEFAULT current_timestamp,
+    user_id uuid NOT NULL,
+    channel_id uuid NOT NULL,
+    body TEXT NOT NULL,
+    embeds jsonb[] NOT NULL DEFAULT [],  /* See https://www.postgresql.org/docs/current/datatype-json.html for justification for using jsonb instead of json */
+    attachments uuid[] NOT NULL DEFAULT []
+);
 
+CREATE TABLE public.roles(
+    id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
+    created_at TIMESTAMP NOT NULL DEFAULT current_timestamp,
+    guild_id uuid NOT NULL,
+    name TEXT NOT NULL,
+    colour VARCHAR(6) NOT NULL DEFAULT '000000',
+    separate_display BOOLEAN NOT NULL DEFAULT FALSE,
+    permissions INT NOT NULL DEFAULT 0
+);
+
+CREATE TABLE public.user_roles(
+    user_id uuid NOT NULL,
+    role_id uuid NOT NULL
+);
+
+CREATE TABLE public.guild_members(
+    user_id uuid NOT NULL,
+    guild_id uuid NOT NULL
+);
+
+CREATE TABLE public.channel_members(
+    user_id uuid NOT NULL,
+    channel_id uuid NOT NULL,
+    permissions INT NOT NULL DEFAULT 0
 );
 
 CREATE TABLE secured.passwords (
@@ -49,6 +96,11 @@ CREATE TABLE secured.two_factor (
     last_updated TIMESTAMP NOT NULL DEFAULT current_timestamp
 );
 
+/* Create checks */
+ALTER TABLE public.channels
+    ADD CONSTRAINT channel_type_check CHECK (type >= 0 AND type <= 2);
+
+
 /* Create Rules */
 CREATE OR REPLACE RULE update_last_updated AS
     ON UPDATE TO secured.passwords
@@ -63,6 +115,8 @@ CREATE OR REPLACE RULE update_last_updated_two_factor AS
     UPDATE secured.two_factor
     SET last_updated = current_timestamp
     WHERE user_id = NEW.user_id;
+
+/* Create relations */
 
 
 /* Apply permissions */
