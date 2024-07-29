d7e2d872e33fae49eb850c86e0cac31e448b5b16
Yuki-42 <kylepannan@gmail.com>
2024-07-30 09:37:16 +1000

    Added some content to secure_handler.py for hashing and verifying passwords

---
+++ api/db/handlers/secure_handler.py
@@ -0,0 +1,53 @@
+"""
+Contains the secure handler.
+"""
+# Standard Library Imports
+
+# Third Party Imports
+
+# Local Imports
+from .base_handler import BaseHandler
+from ..types.user import User
+from ...models.secure import Device, Token
+from ...security.scheme import crypt_context
+
+
+# Constants
+
+
+class SecureHandler(BaseHandler):
+    """
+    Secure handler.
+    """
+
+    @staticmethod
+    def hash_password(
+            password: str
+    ) -> str:
+        """
+        Hash a password.
+
+        Args:
+            password (str): Password.
+
+        Returns:
+            str: Hashed password.
+        """
+        return crypt_context.hash(password)
+
+    @staticmethod
+    def verify_password(
+            password: str,
+            hashed_password: str
+    ) -> bool:
+        """
+        Verify a password.
+
+        Args:
+            password (str): Password.
+            hashed_password (str): Hashed password.
+
+        Returns:
+            bool: Verification.
+        """
+        return crypt_context.verify(password, hashed_password)
