362fa5038bf9d434d0478a41160450fb87034f87
Yuki-42 <kylepannan@gmail.com>
2024-07-30 10:57:25 +1000

    Commit dev code before transition to dev branch

--- api/routes/users_router.py
+++ api/routes/users_router.py
@@ -13,10 +13,13 @@ from pydantic import BaseModel
 # Local Imports
 from ..db.database import Database
 from ..db.types.user import User
-from ..models.user import User as UserModel, PrivateUser as PrivateUserModel
+from ..models.user import User as UserModel
+from ..models.private import PrivateUser as PrivateUserModel
 
 # Constants
-__all__ = ["users_router"]
+__all__ = [
+    "users_router"
+]
 
 # Create API router
 users_router: APIRouter = APIRouter(
@@ -77,3 +80,25 @@ async def create_user(
     return user.to_private()
 
 
+@users_router.post("/login", tags=["users"])
+async def login_user(
+        data: CreateUserData,
+        request: Request
+) -> PrivateUserModel:
+    """
+    Logs in a user.
+    """
+    # Get database connection
+    db: Database = request.state.db
+
+    # Check if the user exists
+    user: User = db.users.email_get(data.email)
+
+    if user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+
+    # Check password
+    if not db.secure.verify_password(data.password, data.password):
+        raise HTTPException(status_code=403, detail="Incorrect password")
+
+    return user.to_private()
