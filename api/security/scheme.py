345da6e61edb310d2a642dc646e7682f7d11fbc7
Yuki-42 <kylepannan@gmail.com>
2024-07-30 12:20:25 +1000

    Added helper functions to scheme to help with encoding and decoding access tokens

--- api/security/scheme.py
+++ api/security/scheme.py
@@ -3,13 +3,16 @@ Contains the security scheme and crypt config for the API.
 """
 
 # Standard Library Imports
+from datetime import timedelta
+from datetime import datetime
 
 # Third Party Imports
 from fastapi.security import OAuth2PasswordBearer
 from passlib.context import CryptContext
+from jwt import encode, decode, PyJWT
 
 # Local Imports
-from ..internals.config import Config
+from ..internals.config import CONFIG
 
 # Constants
 __all__ = [
@@ -20,6 +23,51 @@ __all__ = [
 # Create security scheme
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="users/login")
 
-
 # Create crypt config
 crypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+
+def encode_access_token(
+        data: dict[str, any],
+        expires_delta: int = CONFIG.auth.keyExpires
+) -> PyJWT:
+    """
+    Encodes the provided data into a JWT token.
+
+    Args:
+        data (dict[str, any]): The data to encode.
+        expires_delta (int): The time in seconds for the token to expire.
+
+    Returns:
+        PyJWT: The encoded token.
+    """
+    to_encode: dict[str, any] = data.copy()  # Copy the data to prevent modification, as the data is mutable.
+    to_encode.update(
+        {
+            "exp": datetime.now() + timedelta(seconds=expires_delta)
+        }
+    )
+    return encode(
+        to_encode,
+        CONFIG.auth.secretKey,
+        algorithm="HS256"
+    )
+
+
+def decode_access_token(
+        token: str
+) -> dict[str, any]:
+    """
+    Decodes the provided token into a dictionary.
+
+    Args:
+        token (str): The token to decode.
+
+    Returns:
+        dict[str, any]: The decoded token.
+    """
+    return decode(
+        token,
+        CONFIG.auth.secretKey,
+        algorithms=["HS256"]
+    )
