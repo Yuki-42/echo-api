b6bff03d04825a8866c25ccc5f17ed0492773376
Yuki-42 <kylepannan@gmail.com>
2024-08-01 11:03:07 +1000

    Added file type and handler

---
+++ api/db/handlers/file_handler.py
@@ -0,0 +1,60 @@
+"""
+Contains the Files handler.
+"""
+
+# Standard Library Imports
+from uuid import UUID
+
+# Third Party Imports
+from psycopg import AsyncCursor
+from psycopg.rows import DictRow
+from psycopg.sql import SQL
+
+# Local Imports
+from .base_handler import BaseHandler
+from ..types.file import File
+
+# Constants
+__all__ = [
+    "FilesHandler"
+]
+
+
+class FilesHandler(BaseHandler):
+    """
+    Files handler.
+    """
+    async def id_get(
+            self,
+            file_id: UUID
+    ) -> File | None:
+        """
+        Get file by ID.
+
+        Args:
+            file_id (UUID): File ID.
+
+        Returns:
+            File: File.
+        """
+        # Create a cursor
+        cursor: AsyncCursor
+        async with self.connection.cursor() as cursor:
+            await cursor.execute(
+                SQL(
+                    r"SELECT id, created_at FROM files WHERE id = %s;  /* Only select the unchanging columns, everything else is grabbed on-request */",
+                ),
+                [
+                    file_id,
+                ]
+            )
+            row: DictRow = await cursor.fetchone()
+
+        if row is None:
+            return None
+
+        # Return
+        return File(
+            connection=self.connection,
+            row=row,
+        )
---
+++ api/db/types/file.py
@@ -0,0 +1,84 @@
+"""
+Contains internal file datatype.
+"""
+
+# Standard Library Imports
+from datetime import datetime
+from uuid import UUID
+
+# Third Party Imports
+from psycopg import AsyncConnection
+from psycopg.rows import DictRow
+from psycopg.sql import Identifier
+
+# Local Imports
+from .base_type import BaseType
+from .user import User
+from ...models.file import File as FileModel
+from ...models.user import User as UserModel
+
+# Constants
+__all__ = [
+    "File"
+]
+
+
+class File(BaseType):
+    """
+    File datatype.
+    """
+
+    def __init__(
+            self,
+            connection: AsyncConnection,
+            row: DictRow,
+    ) -> None:
+        """
+        Initialize file.
+
+        Args:
+            connection (AsyncConnection): Connection.
+            row (DictRow): Row.
+        """
+        # Initialize BaseType
+        super(File, self).__init__(connection, row)
+
+        self._table_name = Identifier("files")
+
+    async def to_model(self) -> FileModel:
+        """
+        Convert to model.
+
+        Returns:
+            FileModel: File model.
+        """
+        return FileModel(
+            id=self.id,
+            created_at=self.created_at,
+            created_by=await self.get_created_by(),
+        )
+
+    async def get_created_by_id(self) -> UUID:
+        """
+        Get created by id.
+
+        Returns:
+            UUID: Created by id.
+        """
+        # Get created_by
+        row: DictRow = await self.id_get(
+            column=Identifier("created_by"),
+            id=self.id
+        )
+        return row["created_by"]
+
+    async def get_created_by(self) -> UserModel:
+        """
+        Get created by.
+
+        Returns:
+            User: User.
+        """
+        # Get user object
+        user: User = await self.users.id_get(await self.get_created_by_id())
+        return await user.to_public()
