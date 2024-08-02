379c6278058f3fff08641ecd0d8ebd6bfd47a994
Yuki <kylepannan@gmail.com>
2024-08-02 21:14:51 +1000

    No point trying to test something that is not intended to work yet

--- tests/https/users.py
+++ tests/https/users.py
@@ -12,8 +12,8 @@ from fastapi.testclient import TestClient
 # Local Imports
 from api.api import app
 
+
 # Constants
-client: TestClient = TestClient(app)
 
 
 class TestUsers(TestCase):
@@ -21,38 +21,14 @@ class TestUsers(TestCase):
     Test the HTTPS API endpoint for users.
     """
 
-    def test_unauthorised_get_users(self):
-        """
-        Test the GET method for the users endpoint without authorisation.
-        """
-
-        response: Response = client.get("/users")
-        assert response.status_code == 200
-        assert response.json() == {"message": "GET users"}
-
     def test_post_users(self):
         """
         Test the POST method for the users endpoint.
         """
+        # Create a test client
+        client: TestClient = TestClient(app)
 
+        # Make the request
         response: Response = client.post("/users")
         assert response.status_code == 200
         assert response.json() == {"message": "POST users"}
-
-    def test_put_users(self):
-        """
-        Test the PUT method for the users endpoint.
-        """
-
-        response: Response = client.put("/users")
-        assert response.status_code == 200
-        assert response.json() == {"message": "PUT users"}
-
-    def test_delete_users(self):
-        """
-        Test the DELETE method for the users endpoint.
-        """
-
-        response: Response = client.delete("/users")
-        assert response.status_code == 200
-        assert response.json() == {"message": "DELETE users"}
